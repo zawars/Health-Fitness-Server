2acabacca051ca9777ae1f3fe646c6c77c18456d
Zawar Shahid <zawar_shahid93@hotmail.com>
2021-08-03 18:20:30 +0500

    bugs fixed

--- api/controllers/ConversationController.js
+++ api/controllers/ConversationController.js
@@ -41,9 +41,6 @@ module.exports = {
       res.ok(conversation);
     } else {
       conversation = await Conversation.create(body).fetch();
-      conversation = await Conversation.findOne({
-        id: conversation.id
-      }).populateAll();
       res.ok(conversation);
     }
   },
