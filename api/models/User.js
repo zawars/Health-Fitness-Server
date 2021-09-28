d5bc461c82b8aa4768fc170823336e77e310ed3b
Zawar Shahid <zawar_shahid93@hotmail.com>
2021-09-28 15:22:37 +0500

    user default avatar updated

--- api/models/User.js
+++ api/models/User.js
@@ -25,7 +25,7 @@ module.exports = {
     },
     avatar: {
       type: 'string',
-      defaultsTo: '../assets/congrats.png'
+      defaultsTo: '../assets/empty-user.png'
     },
     password: {
       type: 'string',
