2c74420e309a0f9cee414fc2ad21630bb2303ff0
Saad Afzaal <saadafzal@akkastech.com>
2021-10-12 15:15:01 +0500

    add thumbnail of video

--- api/controllers/VideoLibraryController.js
+++ api/controllers/VideoLibraryController.js
@@ -6,7 +6,11 @@
  */
 
 const fs = require('fs')
-
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffprobe = require('ffprobe-static');
+const ffmpegFluent = require('fluent-ffmpeg');
+ffmpegFluent.setFfmpegPath(ffmpegPath);
+ffmpegFluent.setFfprobePath(ffprobe.path);
 
 module.exports = {
   create: async (req, res) => {
@@ -27,14 +31,39 @@ module.exports = {
         path: uploadedFiles[0].fd
       }).fetch();
 
-      let videoObj = await VideoLibrary.create({
-        title: body.title,
-        notes: body.notes,
-        video: attach.id,
-        user: body.user
-      }).fetch();
+      let thumbnailName = `${attach.fileName.split(".")[0]}-thumbnail.png`;
+      let thumbnailFolder = `${process.cwd().split('\\' + process.cwd().split('\\').pop())[0]}/uploads`;
 
-      res.ok(videoObj);
+      ffmpegFluent(attach.path)
+        .on('end', async function () {
+          console.log('Screenshots taken');
+
+          let thumbnail = await Attachment.create({
+            fileName: thumbnailName,
+            originalName: thumbnailName,
+            path: `${thumbnailFolder}/${thumbnailName}`
+          }).fetch();
+
+          let videoObj = await VideoLibrary.create({
+            title: body.title,
+            notes: body.notes,
+            video: attach.id,
+            thumbnail: thumbnail.id,
+            user: body.user
+          }).fetch();
+
+          res.ok(videoObj);
+        })
+        .on('error', function (err) {
+          console.error(err);
+          res.serverError(err);
+        })
+        .screenshots({
+          // Will take screenshots at 20%, 40%, 60% and 80% of the video
+          count: 1,   // scrren shot count
+          filename: thumbnailName,
+          folder: thumbnailFolder
+        });
     });
 
   },
--- api/models/VideoLibrary.js
+++ api/models/VideoLibrary.js
@@ -25,6 +25,9 @@ module.exports = {
     video: {
       model: 'attachment'
     },
+    thumbnail: {
+      model: 'attachment'
+    },
     user: {
       model: 'user'
     },
